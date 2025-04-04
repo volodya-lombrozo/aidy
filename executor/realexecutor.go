a917f370855a6ef10c13595c9d3a5a023dc4ab08
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-04-04 17:46:13 +0300

    feat(#19): Add RunCommandInDir method to Executor interface and implementations

--- executor/realexecutor.go
+++ executor/realexecutor.go
@@ -2,6 +2,7 @@ package executor
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"os/exec"
 	"strings"
@@ -17,6 +18,21 @@ func (r *RealExecutor) RunCommand(name string, args ...string) (string, error) {
 	err := cmd.Run()
 	if err != nil {
 		return "", err
+	}
+	return out.String(), nil
+}
+
+func (r *RealExecutor) RunCommandInDir(dir string, name string, args ...string) (string, error) {
+	log.Printf("Executing command in directory %s: %s %s", dir, name, strings.Join(args, " "))
+	cmd := exec.Command(name, args...)
+	cmd.Dir = dir
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("error: %v; stderr: %s", err, strings.TrimSpace(stderr.String()))
 	}
 	return out.String(), nil
 }
