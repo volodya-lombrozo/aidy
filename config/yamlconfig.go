50880581630074bf30c27f5d4d265e683247c5be
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-05-05 17:37:40 +0300

    refactor(#140): Rename NewConfig to YamlConfig and update references

--- config/yamlconfig.go
+++ config/yamlconfig.go
@@ -5,19 +5,19 @@ import (
 	"os"
 )
 
-type NewConfig struct {
+type YamlConfig struct {
 	DefaultModel string                       `yaml:"default-model"`
 	APIKeys      map[string]string            `yaml:"api-keys"`
 	Models       map[string]map[string]string `yaml:"models"`
 	Github       string                       `yaml:"github-api-key"`
 }
 
-func NewConf(filepath string) *NewConfig {
+func YamlConf(filepath string) *YamlConfig {
 	configData, err := os.ReadFile(filepath)
 	if err != nil {
 		panic(err)
 	}
-	var config NewConfig
+	var config YamlConfig
 	err = yaml.Unmarshal(configData, &config)
 	if err != nil {
 		panic(err)
@@ -25,19 +25,19 @@ func NewConf(filepath string) *NewConfig {
 	return &config
 }
 
-func (c *NewConfig) GetOpenAIAPIKey() (string, error) {
+func (c *YamlConfig) GetOpenAIAPIKey() (string, error) {
 	return c.APIKeys["openai"], nil
 }
 
-func (c *NewConfig) GetGithubAPIKey() (string, error) {
+func (c *YamlConfig) GetGithubAPIKey() (string, error) {
 	return c.APIKeys["github"], nil
 }
 
-func (c *NewConfig) GetModel() (string, error) {
+func (c *YamlConfig) GetModel() (string, error) {
 	model := c.DefaultModel
 	return c.Models[model]["model-id"], nil
 }
 
-func (c *NewConfig) GetDeepseekAPIKey() (string, error) {
+func (c *YamlConfig) GetDeepseekAPIKey() (string, error) {
 	return c.APIKeys["deepseek"], nil
 }
